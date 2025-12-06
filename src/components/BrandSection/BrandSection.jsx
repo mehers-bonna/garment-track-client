0e52c233f9299e07d1e5d062e241f64213d53003
mehers-bonna <mehersbonna17131713@gmail.com>
2025-12-06 14:05:59 -0600

    added brandsection.jsx

---
+++ src/components/BrandSection/BrandSection.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { motion } from "framer-motion";
+import brand1 from "../../assets/brand-1.png";
+import brand2 from "../../assets/brand-2.png";
+import brand3 from "../../assets/brand-3.png";
+import brand4 from "../../assets/brand-4.png";
+
+const BrandsSection = () => {
+  const brands = [
+    { id: 1, img: brand1 },
+    { id: 2, img: brand2 },
+    { id: 3, img: brand3 },
+    { id: 4, img: brand4 },
+  ];
+
+  return (
+    <div className="w-full py-12 bg-gray-100 overflow-hidden rounded-3xl bg-lime-50">
+      <h2 className="text-center text-3xl md:text-4xl font-bold mb-8">
+        Our Trusted Brands
+      </h2>
+
+      {/* Auto Scrolling Marquee */}
+      <div className="relative w-full">
+        <motion.div
+          className="flex gap-16 items-center"
+          animate={{ x: [0, -1200] }}
+          transition={{
+            repeat: Infinity,
+            duration: 18,
+            ease: "linear",
+          }}
+        >
+          {[...brands, ...brands].map((brand, index) => (
+            <img
+              key={index}
+              src={brand.img}
+              alt="brand"
+              className="w-20 h-20 grayscale hover:grayscale-0 transition duration-300"
+            />
+          ))}
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default BrandsSection;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router'
 import Footer from '../components/Shared/Footer/Footer'
 import Navbar from '../components/Shared/Navbar/Navbar'
 import Banner from '../components/Banner/Banner'
+import BrandsSection from '../components/BrandSection/BrandSection'
 const MainLayout = () => {
   return (
     <div>
@@ -12,6 +13,9 @@ const MainLayout = () => {
       </div>
       <div className=' min-h-[calc(100vh-68px)]'>
         <Outlet />
+      </div>
+      <div className='w-9/12 mx-auto'>
+        <BrandsSection></BrandsSection>
       </div>
       <Footer />
     </div>
